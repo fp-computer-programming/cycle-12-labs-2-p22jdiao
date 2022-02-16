21620ff2cf6a4f4440502ed5741722c65dca91d9
p22jdiao <p22jdiao@fairfieldprep.org>
2022-02-16 09:59:05 -0500

    finished

---
+++ lab_12-3.py
@@ -0,0 +1,9 @@
+# Author: JD 02/16/2022
+
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+weekdends = ("Saturday", "Sunday")
+
+days_of_the_week = weekdays + weekdends
+
+print(days_of_the_week)
---
+++ lab_12-4.py
@@ -0,0 +1,6 @@
+# Author: JD 02/16/2022
+
+day_date = ("2","3","4","5","6")
+(math, english, enrichment, social_science, science) = day_date
+
+print(math, english, enrichment, social_science, science)
