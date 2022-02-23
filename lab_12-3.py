8da126ee16a0eb87ca2bfa0de1cc0583613a6246
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2022-02-23 10:16:37 -0500

    Update lab_12-3.py

--- lab_12-3.py
+++ lab_12-3.py
@@ -1,9 +1,9 @@
 # Author: JD 02/16/2022
-
+# The first tuple is named as weekdays, the second is weekends
 weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 
-weekdends = ("Saturday", "Sunday")
-
-days_of_the_week = weekdays + weekdends
-
+weekends = ("Saturday", "Sunday")
+# The third is named as days of the week, and it is the "sum" of the first two variables.
+days_of_the_week = weekdays + weekends
+# Printing the third variable by using a print() statement.
 print(days_of_the_week)
