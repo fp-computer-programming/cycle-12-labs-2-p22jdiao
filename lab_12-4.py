f9571166f0c7ca9c13bbf38fa4fd76ce2a234fce
p22jdiao <90051677+p22jdiao@users.noreply.github.com>
2022-02-23 10:19:56 -0500

    Update lab_12-4.py

--- lab_12-4.py
+++ lab_12-4.py
@@ -1,6 +1,6 @@
 # Author: JD 02/16/2022
-
+# Creating the variable as a tuple, then assigning the subjects to each individual item within that tuple.
 day_date = ("2","3","4","5","6")
 (math, english, enrichment, social_science, science) = day_date
-
+# After the assignment, printing the dates by printing the assigned subjects.
 print(math, english, enrichment, social_science, science)
